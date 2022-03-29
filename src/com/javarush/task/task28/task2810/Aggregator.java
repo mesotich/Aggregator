78772db455e6b4467e7d4ff8fb18c577dc7d2631
Dmitry Shishkin <4144442@bk.ru>
2022-03-29 13:15:30 +0300

    backup.html, vacancies.html

--- src/com/javarush/task/task28/task2810/Aggregator.java
+++ src/com/javarush/task/task28/task2810/Aggregator.java
@@ -4,7 +4,7 @@ import com.javarush.task.task28.task2810.model.HHStrategy;
 import com.javarush.task.task28.task2810.model.Model;
 import com.javarush.task.task28.task2810.model.Provider;
 import com.javarush.task.task28.task2810.view.HtmlView;
-import com.javarush.task.task28.task2810.view.View;
+
 
 public class Aggregator {
 
